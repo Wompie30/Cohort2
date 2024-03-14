91cc5003cbac0fba5a48acf977aa811dd4fabc9a
Vivek Shukla <vivekshukla3011@gmail.com>
2024-03-14 22:34:19 +0530

    Middleware Assignment Questions

---
+++ Week 03/Assignment/midwareAssignment.js
@@ -0,0 +1,10 @@
+                                // Middleware Assignment
+
+/*
+    # Middlewares -> 
+
+        01 - You have to create a few standard middleware of your application.
+        02 - You have to create a middleware for logging the number of requests on a Server.
+        03 - You have to create a middlware for rate limiting a user request based on their username passed in header.
+        04 - You have to create a middleware for logging the number of errors on a Server.
+*/
