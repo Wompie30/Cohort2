c9a6d1165eb2fa3426be82cf277b0f666442c2a3
Vivek Shukla <vivekshukla3011@gmail.com>
2024-03-17 22:38:00 +0530

    JWT Assignment

---
+++ Week 03/Assignment/jwtAssignment.js
@@ -0,0 +1,14 @@
+
+
+/*
+    # JWTs - 
+        -> Write a function that takes in a username and password and returns a JWT token with
+        the username encoded, should return null if the username is not a valid email or if the
+        password is less than 6 characters. Try using the "zod" library here.
+
+        -> Write a function that makes a jwt as input and returns true if the jwt can be decoded
+        ( not verified ). Return false otherwise.
+
+        -> Write a function that takes a jwt as input and returns true if the jwt can be 
+        VERIFIED.
+ */
